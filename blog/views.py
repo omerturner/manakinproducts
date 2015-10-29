702e154ad9bb09a0034cf851a2fec5c81f485246
omerturner <omerturner@gmail.com>
2015-10-29 20:04:17 +0200

    Fixed media-url in blog.html for production

--- blog/views.py
+++ blog/views.py
@@ -6,5 +6,5 @@ from products.models import Product
 
 def show_posts(request):   
     posts = Post.objects.order_by('-posted')[0:3]
-    product = Product.objects.all()[0]
+    product = Product.objects.order_by('order')[0]
     return render(request, 'blog/blog.html', {'posts': posts, 'product': product})
