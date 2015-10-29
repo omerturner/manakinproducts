52d953c9e6565687718c0a08f396a9a7529293cd
omerturner <omerturner@gmail.com>
2015-10-29 19:25:28 +0200

    Added Product to blog.views

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
 from .models import Post
+from products.models import Product
+
 
 def show_posts(request):   
     posts = Post.objects.order_by('-posted')[0:3]
-    return render(request, 'blog/blog.html', {'posts': posts})
+    product = Product.objects.all()[0]
+    return render(request, 'blog/blog.html', {'posts': posts, 'product': product})
