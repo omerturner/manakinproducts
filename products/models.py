7e23b63d510ac84226d461a0e57f7c109d8ce88a
omert <buzzilla.omerturner@gmail.com>
2015-12-20 15:18:23 +0200

    Updated product-detail page

--- products/models.py
+++ products/models.py
@@ -5,7 +5,10 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=255)
     description = models.TextField()
-    short_description = models.TextField()
+    short_description = models.TextField()    
+    features = models.TextField()
+    size = models.CharField(max_length=25, blank=True)
+    color = models.CharField(max_length=25, blank=True)
     amz_link = models.URLField(max_length=255)
     order = models.IntegerField(default=999)
     image = models.ImageField(upload_to='', default='./no_image.png')
@@ -14,6 +17,7 @@ class Product(models.Model):
     image4 = models.ImageField(upload_to='', default='./no_image.png')
     image5 = models.ImageField(upload_to='', default='./no_image.png')
     image6 = models.ImageField(upload_to='', default='./no_image.png')
+    
 
     def __str__(self):
         return self.title
